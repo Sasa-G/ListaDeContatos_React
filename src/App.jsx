2e0ef355645ae5ee0018f3988df9dff88b0a136c
Samanta Gomes De Morais Santos <samantagdms@gmail.com>
2024-05-18 22:11:48 -0300

    Primeiro Commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import ListaDeContatos from "./components/ListaDeContatos";
+
+function App(){
+  return(
+    <div className='App'>
+      <ListaDeContatos/>
+
+    </div>
+  )
+}
+
+export default App;
